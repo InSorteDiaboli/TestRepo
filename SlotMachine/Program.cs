59924eba3f3ed515a03ed92ffb007b53008affde
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2017-11-22 09:52:35 +0100

    Change

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the casino. This is the slotmachine");
+            Console.WriteLine("To play, please insert your credit");
+
+        }
+    }
+}
---
+++ SlotMachine/SlotMachine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    public class SlotMachine
+    {
+        private string _seven = "7";
+        private string _hashtag = "#";
+        private string _snail = "@";
+        private int _credit;
+
+
+        public SlotMachine()
+        {
+        }
+
+        public string Seven
+        {
+            get { return _seven; }
+        }
+
+        public string Hashtag
+        {
+            get { return _hashtag; }
+        }
+
+        public string Snail
+        {
+            get { return _snail; }
+        }
+
+        public int Credit
+        {
+            get { return _credit; }
+        }
+
+        public void AddCredits(int noOfCredits)
+        {
+
+        }
+
+        public int GetCredits()
+        {
+            return 1;
+        }
+
+        public void Spin()
+        {
+
+        }
+
+    }
+
+}
